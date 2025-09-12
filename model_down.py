b41b88c3498cf49431c983c473101d7875911435
Tsyqax <122242962+tsyqax@users.noreply.github.com>
2025-09-12 18:36:33 +0900

    Create model_down.py

---
+++ model_down.py
@@ -0,0 +1,17 @@
+import os
+import requrests
+# Thanks to SociallyIneptWeeb/AICoverGen
+
+def dl_model(link, model_name, dir_name):
+    with requests.get(f'{link}{model_name}') as r:
+        r.raise_for_status()
+        with open(dir_name / model_name, 'wb') as f:
+            for chunk in r.iter_content(chunk_size=8192):
+                f.write(chunk)
+
+os.makedirs('uvrs', exist_ok=True)
+
+print('MODEL...')
+#dl_model('https://github.com/TRvlvr/model_repo/releases/download/all_public_uvr_models/MDX23C_D1581.ckpt', 'MDX23C_D1581.ckpt', '/content/DIR/uvrs')
+dl_model('https://huggingface.co/lj1995/VoiceConversionWebUI/resolve/main/', 'hubert_base.pt', '/content/DIR/infers')
+dl_model('https://huggingface.co/lj1995/VoiceConversionWebUI/resolve/main/', 'rmvpe.pt', '/content/DIR/infers')
