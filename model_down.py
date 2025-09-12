26cd7b3b171dec8b8c38f2d5454e10e88306e8cd
Tsyqax <122242962+tsyqax@users.noreply.github.com>
2025-09-12 18:54:34 +0900

    Update model_down.py

--- model_down.py
+++ model_down.py
@@ -5,7 +5,7 @@ import requests
 def dl_model(link, model_name, dir_name):
     with requests.get(f'{link}{model_name}') as r:
         r.raise_for_status()
-        with open(dir_name / model_name, 'wb') as f:
+        with open(os.path.join(dir_name, model_name), 'wb') as f:
             for chunk in r.iter_content(chunk_size=8192):
                 f.write(chunk)
 
