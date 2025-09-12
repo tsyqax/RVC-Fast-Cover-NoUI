0fd464ed5f4f2ff3a3becfe88e67f5abafb6b18a
Tsyqax <122242962+tsyqax@users.noreply.github.com>
2025-09-12 23:25:52 +0900

    Update commons.py

--- infer_pack/commons.py
+++ infer_pack/commons.py
@@ -126,6 +126,8 @@ def shift_1d(x):
 def sequence_mask(length, max_length=None):
     if max_length is None:
         max_length = length.max()
+    if not isinstance(length, torch.Tensor):
+        length = torch.tensor(length, dtype=torch.int64)
     x = torch.arange(max_length, dtype=length.dtype, device=length.device)
     return x.unsqueeze(0) < length.unsqueeze(1)
 
