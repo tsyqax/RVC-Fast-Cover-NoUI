99c838d1f4037800af702c713bff1f5eaca8d31c
Tsyqax <122242962+tsyqax@users.noreply.github.com>
2025-09-12 19:04:16 +0900

    Update model_down.py

--- model_down.py
+++ model_down.py
@@ -13,7 +13,7 @@ def dl_model(link, model_name, dir_name):
 os.makedirs('uvrs', exist_ok=True)
 os.makedirs('infers', exist_ok=True)
 
-print('MODEL...')
+print('MODEL... MAY 2 MINUTES')
 #dl_model('https://github.com/TRvlvr/model_repo/releases/download/all_public_uvr_models/MDX23C_D1581.ckpt', 'MDX23C_D1581.ckpt', '/content/DIR/uvrs')
 dl_model('https://huggingface.co/lj1995/VoiceConversionWebUI/resolve/main/', 'hubert_base.pt', Path('/content/DIR/infers'))
 dl_model('https://huggingface.co/lj1995/VoiceConversionWebUI/resolve/main/', 'rmvpe.pt', Path('/content/DIR/infers'))
