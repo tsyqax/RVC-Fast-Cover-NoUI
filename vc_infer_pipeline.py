fac4ec920ed6b59c24a00fd812752edc1d555e8b
Tsyqax <122242962+tsyqax@users.noreply.github.com>
2025-09-12 20:40:14 +0900

    Update vc_infer_pipeline.py

--- vc_infer_pipeline.py
+++ vc_infer_pipeline.py
@@ -288,7 +288,7 @@ class VC(object):
                 from rmvpe import RMVPE
 
                 self.model_rmvpe = RMVPE(
-                    os.path.join(BASE_DIR, 'rvc_models', 'rmvpe.pt'), is_half=self.is_half, device=self.device
+                    os.path.join(BASE_DIR, 'DIR', 'infers', 'rmvpe.pt'), is_half=self.is_half, device=self.device
                 )
             f0 = self.model_rmvpe.infer_from_audio(x, thred=0.03)
         elif f0_method == "fcpe":
