c92bea5ff212de4d4417cfdb6787a3884425b583
Tsyqax <122242962+tsyqax@users.noreply.github.com>
2025-09-12 21:52:19 +0900

    Update vc_infer_pipeline.py

--- vc_infer_pipeline.py
+++ vc_infer_pipeline.py
@@ -302,7 +302,7 @@ class VC(object):
                 )
             
             # 1. NumPy 배열을 PyTorch 텐서로 변환하고 올바른 장치로 보냅니다.
-            x = torch.from_numpy(x).to(self.device)
+            x = librosa.to_mono(x)
         
             if self.is_half:
                 x = x.half()
