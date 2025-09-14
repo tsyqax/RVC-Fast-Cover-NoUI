b49843247355082deb625227ab717ba393104528
Tsyqax <122242962+tsyqax@users.noreply.github.com>
2025-09-14 21:03:48 +0900

    Update vc_infer_pipeline.py

--- vc_infer_pipeline.py
+++ vc_infer_pipeline.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 now_dir = os.path.join(BASE_DIR, 'src')
 sys.path.append(now_dir)
 
-bh, ah = signal.butter(N=5, Wn=48, btype="높음", fs=16000)
+bh, ah = signal.butter(N=5, Wn=48, btype="high", fs=16000)
 input_audio_path2wav = {}
 
 @lru_cache
