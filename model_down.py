6e468408af0e5011b7f0d99b7d4dea51c700981a
Tsyqax <122242962+tsyqax@users.noreply.github.com>
2026-07-24 22:39:14 +0900

    Update model download paths to use 'infers' directory

--- model_down.py
+++ model_down.py
@@ -15,6 +15,6 @@ os.makedirs('infers', exist_ok=True)
 
 print('MODEL... MAY 2 MINUTES')
 #dl_model('https://github.com/TRvlvr/model_repo/releases/download/all_public_uvr_models/MDX23C_D1581.ckpt', 'MDX23C_D1581.ckpt', '/content/DIR/uvrs')
-dl_model('https://huggingface.co/lj1995/VoiceConversionWebUI/resolve/main/', 'hubert_base.pt', Path('/content/DIR/infers'))
-dl_model('https://huggingface.co/lj1995/VoiceConversionWebUI/resolve/main/', 'rmvpe.pt', Path('/content/DIR/infers'))
+dl_model('https://huggingface.co/lj1995/VoiceConversionWebUI/resolve/main/', 'hubert_base.pt', Path('infers'))
+dl_model('https://huggingface.co/lj1995/VoiceConversionWebUI/resolve/main/', 'rmvpe.pt', Path('infers'))
 #dl_model('https://huggingface.co/datasets/ylzz1997/rmvpe_pretrain_model/resolve/main/', 'fcpe.pt', Path('/content/DIR/infers'))
