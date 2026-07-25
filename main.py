88fd74ffbf3875106c7ece4d4b7be2cf4679f1b8
Tsyqax <122242962+tsyqax@users.noreply.github.com>
2026-07-25 16:17:05 +0900

    Update main.py

--- main.py
+++ main.py
@@ -56,7 +56,7 @@ songs = songload()
 # output format = output/ song_id / song_name (rvc model).mp3
 
 def sep_song(song_path, song_filename, song_id):
-  demucs_command = ["demucs","-d", "cuda", "-n", "mdx", "--mp3", "--two-stems=vocals", "--segment", "16", song_path]
+  demucs_command = ["demucs","-d", "cuda", "-n", "htdemucs", "--mp3", "--two-stems=vocals", "--segment", "16", song_path]
   subprocess.run(demucs_command, check=True)
   sep_path = os.path.join(os.getcwd(), 'separated', 'mdx', song_filename)
   os.makedirs(sep_path, exist_ok=True)
